cdec27a47c8ae67a3ba8f2404864d6a152f2c5d5
Mehul2001 <mmandviya@mail.csuchico.edu>
2019-01-09 22:54:14 -0800

    Implement blinkyDancer and dancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -22,22 +22,22 @@
 // makeDancer - superclass
 // blinkydancer - subclass
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = new makeDancer(top, left, timeBetweenSteps);
-
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-
-  var oldStep = blinkyDancer.step;
-
-
-  return blinkyDancer;
+  //var blinkyDancer = new makeDancer(top, left, timeBetweenSteps);
+  // this = the new blinky dancer
+  makeDancer.call(this, top, left, timeBetweenSteps);                                               
 };
 
-blinkyDancer.prototype.step = function() {
+makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
+makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+
+makeBlinkyDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
-  oldStep();
+  // call that property
+  //oldStep();
+  makeDancer.prototype.step.call(this); 
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
-  blinkyDancer.$node.toggle();
+  
+  this.$node.toggle();
 };
